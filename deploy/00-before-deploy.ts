bc4d78e89e67d3b452dc19d636a6beb7ee139699
sumit <sumitsharma.intigate@gmail.com>
2023-12-20 18:17:33 +0530

    radiant-deploy

---
+++ deploy/00-before-deploy.ts
@@ -0,0 +1,21 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { getWalletBalances, isTestnetMarket, loadPoolConfig } from "../helpers";
+import { parseEther } from "ethers/lib/utils";
+import { MARKET_NAME } from "../helpers/env";
+
+/**
+ * The following script runs before the deployment starts
+ */
+
+const func: DeployFunction = async function () {
+  console.log("hello");
+  const balances = await getWalletBalances();
+  console.log("\nAccounts");
+  console.log("========");
+  console.table(balances);
+};
+
+func.tags = ["before-deploy"];
+
+export default func;
